ab584c60cd409fa196a293d5ccfbf7b25081cd24
Natanael <natanaelgplay@gmail.com>
2021-03-18 20:27:31 -0300

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
+sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
 
 VERSION="$(dpkg-parsechangelog -S Version)"
 debuild -b -uc -us -nc

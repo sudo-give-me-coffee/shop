f04cc184510a4a419106d9dec3f9288b500e61a6
Natanael <natanaelgplay@gmail.com>
2021-03-18 20:17:22 -0300

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
-killall io.elementary.appcenter || true
+sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
+
 VERSION="$(dpkg-parsechangelog -S Version)"
 debuild -b -uc -us -nc
-sudo dpkg -i "../pop-shop_${VERSION}_amd64.deb"
-io.elementary.appcenter
+
+cd ..
+ls
+
+#sudo dpkg -i "../pop-shop_${VERSION}_amd64.deb"
+
